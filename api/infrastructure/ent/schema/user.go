53630c61046cd960e96ab07b29173e0bab219dbf
sugimoto_e <eikun1014@gmail.com>
2024-09-19 00:02:12 +0900

    feat: add user

--- api/infrastructure/ent/schema/user.go
+++ api/infrastructure/ent/schema/user.go
@@ -1,6 +1,9 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -9,7 +12,12 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("username").Unique(),
+		field.String("password"),
+		field.String("email").Unique(),
+		field.Time("created_at").Immutable(),
+	}
 }
 
 // Edges of the User.
