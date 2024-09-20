71461390728d93f116e3c43dc5de0fce296a1185
ei-sugimoto <eikun1014@gmail.com>
2024-09-20 14:11:53 +0900

    feat: new Project

---
+++ api/infrastructure/ent/schema/project.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Project holds the schema definition for the Project entity.
+type Project struct {
+	ent.Schema
+}
+
+// Fields of the Project.
+func (Project) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name"),
+	}
+}
+
+// Edges of the Project.
+func (Project) Edges() []ent.Edge {
+	return nil
+}
